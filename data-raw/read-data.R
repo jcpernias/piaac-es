ee4df827fc370bfecdd2337604af5543fd403ef9
José C. Pernías <jcpernias@gmail.com>
2025-03-16 19:59:28 +0100

    Read data for both cycles

--- data-raw/read-data.R
+++ data-raw/read-data.R
@@ -1,38 +1,20 @@
 library(dplyr)
-library(stringr)
-library(openxlsx2)
+library(readr)
+library(glue)
+library(purrr)
 
-# Archivos con la descripción de las variables
-cb_cycle1 <- "data-raw/International Codebook_PIAAC Public-use File (PUF) Variables and Values_Feb2023.xlsx"
-cb_cycle2 <- "data-raw/piaac-cy2-international-codebook.xlsx"
+get_col_types <- function(cycle) {
+  fname <- glue("data-raw/col_codes{cycle}.csv")
+  tab <- read_csv(fname, col_types = "icc")
+  tab$type |> as.list() |> setNames(tab$name)
+}
 
-# Dominios de variables a ignorar
-ignore_domains <-
-  c("Literacy", "Numeracy", "Problem", "Reading", "Adaptive", "Observation") |>
-  paste(collapse = "|")
+# Lee los datos del primer ciclo
+cols1 <- get_col_types(1)
+esp1 <- read_csv("data-raw/prgespp1.csv.gz", col_types = cols1)
 
-# Lee las descripciones de las variables
-cycle1 <- read_xlsx(cb_cycle1, skip_empty_cols = TRUE) |>
-  filter(str_starts(Domain, ignore_domains, negate = TRUE)) |>
-  filter(str_starts(Name, "SPFWT|PV", negate = TRUE))
-cycle2 <- read_xlsx(cb_cycle2) |>
-  filter(str_starts(Domain, ignore_domains, negate = TRUE)) |>
-  filter(str_starts(Variable, "SPFWT|PV", negate = TRUE))
+# Lee los datos del primer ciclo
+cols1 <- get_col_types(2)
+esp2 <- read_delim("data-raw/prgespp2.csv.gz", col_types = cols2,
+                   delim = ";", na = ".")
 
-common_names <- intersect(cycle1$Name, cycle2$Variable)
-
-comon1 <- cycle1 |> filter(Name %in% common_names)
-comon2 <- cycle2 |> filter(Variable %in% common_names)
-
-
-ed1 <- cycle1 |>
-  filter(str_detect(Label, "ducation"))
-
-ed2 <- cycle2 |>
-  filter(str_detect(Label, "ducation"))
-
-
-
-cycle1_domains <- unique(cycle1$Domain) |> sort()
-cycle2_domains <- unique(cycle2$Domain) |> sort()
-common_domains <- intersect(cycle1_domains, cycle2_domains) |> sort()
