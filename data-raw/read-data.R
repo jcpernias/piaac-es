ab2f3deef12ac1aa103ffead4352a4501f220e28
José C. Pernías <jcpernias@gmail.com>
2025-03-17 15:31:28 +0100

    Add type of contract variable

--- data-raw/read-data.R
+++ data-raw/read-data.R
@@ -21,7 +21,7 @@ esp2 <- read_delim("data-raw/prgespp2.csv.gz", col_types = cols2,
 
 common_vars <- c("SEQID", "GENDER_R", "AGE_R", "PVLIT1", "PVNUM1")
 vars1 <- c("EARNHR", "EDCAT7", "C_Q07_T", "C_D05",
-           "C_Q09", "D_Q03", "D_Q04", "D_Q05a1",
+           "C_Q09", "D_Q03", "D_Q04", "D_Q05a1", "D_Q09",
            common_vars)
 db1 <- esp1 |>
   select(all_of(vars1)) |>
@@ -33,10 +33,13 @@ db1 <- esp1 |>
          D_Q03 = parse_integer(D_Q03, na = c("V", "R", "D", "N")),
          D_Q04 = parse_integer(D_Q04, na = c("V", "N", "R", "D")),
          D_Q05a1 = parse_integer(D_Q05a1, na = c("V", "N", "R", "D")),
-  )
+         D_Q09 = parse_integer(D_Q09, na = c("V", "N", "R", "D")),
+         D2_Q10_C = if_else(D_Q09 >= 3, 3, D_Q09),
+  ) |>
+  select(-D_Q09)
 
 vars2 <- c("EARNHRC2", "EDCAT7_TC1", "C2_Q07_T", "C2_D05",
-           "C2_Q10", "D2_Q03", "D2_Q04", "D2_Q05a1",
+           "C2_Q10", "D2_Q03", "D2_Q04", "D2_Q05a1", "D2_Q10_C",
            common_vars, "PVAPS1")
 db2 <- esp2 |>
   select(all_of(vars2)) |>
@@ -45,6 +48,7 @@ db2 <- esp2 |>
          D2_Q03 = parse_integer(D2_Q03, na = c(".v", ".r", ".d")),
          D2_Q04 = parse_integer(D2_Q04, na = c(".v", ".d", ".r")),
          D2_Q05a1 = parse_integer(D2_Q05a1, na = c(".v", ".r", ".d")),
+         D2_Q10_C = parse_integer(D2_Q10_C, na = c(".v", ".r", ".d")),
   ) |>
   rename(EARNHR = EARNHRC2,
          EDCAT7 = EDCAT7_TC1,
