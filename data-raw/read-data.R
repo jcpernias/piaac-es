6c56f3db119d717b229f962dd9483331697d414f
José C. Pernías <jcpernias@gmail.com>
2025-03-17 07:43:42 +0100

    Add economic sector of current work

--- data-raw/read-data.R
+++ data-raw/read-data.R
@@ -21,7 +21,7 @@ esp2 <- read_delim("data-raw/prgespp2.csv.gz", col_types = cols2,
 
 common_vars <- c("SEQID", "GENDER_R", "AGE_R", "PVLIT1", "PVNUM1")
 vars1 <- c("EARNHR", "EDCAT7", "C_Q07_T", "C_D05",
-           "C_Q09",
+           "C_Q09", "D_Q03",
            common_vars)
 db1 <- esp1 |>
   select(all_of(vars1)) |>
@@ -30,20 +30,23 @@ db1 <- esp1 |>
          C_Q07_T = parse_integer(C_Q07_T, na = c("N", "M")),
          C_D05 = parse_integer(C_D05, na = "N"),
          C_Q09 = parse_integer(C_Q09, na = c("N", "V", "D", "R")),
+         D_Q03 = parse_integer(D_Q03, na = c("V", "R", "D", "N")),
   )
 
 vars2 <- c("EARNHRC2", "EDCAT7_TC1", "C2_Q07_T", "C2_D05",
-           "C2_Q10",
+           "C2_Q10", "D2_Q03",
            common_vars, "PVAPS1")
 db2 <- esp2 |>
   select(all_of(vars2)) |>
   mutate(EARNHRC2 = parse_double(EARNHRC2, na = ".v"),
          C2_Q10 = parse_integer(C2_Q10, na = c(".v", ".d", ".r")),
+         D2_Q03 = parse_integer(D2_Q03, na = c(".v", ".r", ".d")),
   ) |>
   rename(EARNHR = EARNHRC2,
          EDCAT7 = EDCAT7_TC1,
          C_Q07_T = C2_Q07_T,
          C_D05 = C2_D05,
          C_Q09 = C2_Q10,
+         D_Q03 = D2_Q03,
          )
 
