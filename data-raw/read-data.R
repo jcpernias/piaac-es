c52bcf89041e27895a856356ed2f6ca43151385c
José C. Pernías <jcpernias@gmail.com>
2025-03-17 16:28:18 +0100

    Add education level required for current work

--- data-raw/read-data.R
+++ data-raw/read-data.R
@@ -22,6 +22,7 @@ esp2 <- read_delim("data-raw/prgespp2.csv.gz", col_types = cols2,
 common_vars <- c("SEQID", "GENDER_R", "AGE_R", "PVLIT1", "PVNUM1")
 vars1 <- c("EARNHR", "EDCAT7", "C_Q07_T", "C_D05",
            "C_Q09", "D_Q03", "D_Q04", "D_Q05a1", "D_Q09",
+           "D_Q12a",
            common_vars)
 db1 <- esp1 |>
   select(all_of(vars1)) |>
@@ -35,11 +36,13 @@ db1 <- esp1 |>
          D_Q05a1 = parse_integer(D_Q05a1, na = c("V", "N", "R", "D")),
          D_Q09 = parse_integer(D_Q09, na = c("V", "N", "R", "D")),
          D2_Q10_C = if_else(D_Q09 >= 3, 3, D_Q09),
+         D_Q12a = parse_integer(D_Q12a, na = c("V", "N", "R", "D")),
   ) |>
   select(-D_Q09)
 
 vars2 <- c("EARNHRC2", "EDCAT7_TC1", "C2_Q07_T", "C2_D05",
            "C2_Q10", "D2_Q03", "D2_Q04", "D2_Q05a1", "D2_Q10_C",
+           "D2_Q12a_TC1",
            common_vars, "PVAPS1")
 db2 <- esp2 |>
   select(all_of(vars2)) |>
@@ -49,6 +52,7 @@ db2 <- esp2 |>
          D2_Q04 = parse_integer(D2_Q04, na = c(".v", ".d", ".r")),
          D2_Q05a1 = parse_integer(D2_Q05a1, na = c(".v", ".r", ".d")),
          D2_Q10_C = parse_integer(D2_Q10_C, na = c(".v", ".r", ".d")),
+         D2_Q12a_TC1 = parse_integer(D2_Q12a_TC1, na = c(".v", ".r", ".d")),
   ) |>
   rename(EARNHR = EARNHRC2,
          EDCAT7 = EDCAT7_TC1,
