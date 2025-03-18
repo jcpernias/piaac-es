3851b5f2c58834a4dd33a7d51b8075a8cf7b92c0
José C. Pernías <jcpernias@gmail.com>
2025-03-18 13:24:28 +0100

    Delete unused function

--- R/functions.R
+++ R/functions.R
@@ -1,14 +1,3 @@
-# Determina el tipo de columna a partir de la conjeturas de readr
-get_type <- function(x) {
-  col_type_codes <- c("collector_double" = "d",
-                      "collector_character" = "c",
-                      "collector_logical" = "l",
-                      "collector_number" = "c")
-  cls <- class(x)[1]
-  col_type_codes[cls] |> unname()
-}
-
-
 # Lee fichero con datos
 read_puf <- function(file, delim = ",", na = c("", "NA")) {
   read_delim(file, delim = delim, na = na,
